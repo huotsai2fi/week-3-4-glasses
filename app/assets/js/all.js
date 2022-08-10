f19d1d76c8abf5b052b1c8da7abfa16fbe15734b
April Hung <huotsai2fi@gmail.com>
2022-08-10 21:42:52 +0800

    feat: header

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,24 @@
-console.log('Hello!');
+const pc = window.matchMedia('(min-width: 993px)');
+const tablet = window.matchMedia('(min-width: 768px) and (max-width: 992px)');
+const moblie = window.matchMedia('(max-width: 767px)');
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+//重設漢堡選單
+function resetMenu(mql) {
+  if (!mql.matches) {
+    $('.toggleDropdown').children('span').text('menu');
+    $('.dropdown').removeClass('active');
+  }
+}
+
+resetMenu(moblie);
+moblie.addEventListener('change', resetMenu);
+
+//漢堡選單
+$('.toggleDropdown').on('click', function () {
+  $(this)
+    .children('span')
+    .text((i, text) => {
+      return text === 'menu' ? 'close' : 'menu';
+    });
+  $('.dropdown').toggleClass('active');
 });

fce90854010b9f974c51118dda0a0048ec36cc64
April Hung <huotsai2fi@gmail.com>
2022-08-17 18:24:33 +0800

    Update 2022-08-17T10:24:30.919Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,23 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+var pc = window.matchMedia('(min-width: 993px)');
+var tablet = window.matchMedia('(min-width: 768px) and (max-width: 992px)');
+var moblie = window.matchMedia('(max-width: 767px)'); //重設漢堡選單
+
+function resetMenu(mql) {
+  if (!mql.matches) {
+    $('.toggleDropdown').children('span').text('menu');
+    $('.dropdown').removeClass('active');
+  }
+}
+
+resetMenu(moblie);
+moblie.addEventListener('change', resetMenu); //漢堡選單
+
+$('.toggleDropdown').on('click', function () {
+  $(this).children('span').text(function (i, text) {
+    return text === 'menu' ? 'close' : 'menu';
+  });
+  $('.dropdown').toggleClass('active');
 });
 //# sourceMappingURL=all.js.map
